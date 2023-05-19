5a21c41b86cbc2b7898b64c65aeb5dced7c2edb7
carveyh <carvey.hor@gmail.com>
2023-05-19 15:12:40 -0400

    Before adding hover effect

--- practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
+++ practice-for-sprint-15-react-redux-pet-rating-main/frontend/src/components/PawsRatingInput.js
@@ -1,12 +1,42 @@
+import { useState } from "react";
+
 const PawsRatingInput = ({ rating, disabled, onChange }) => {
+  const [activeRating, setActiveRating] = useState(rating);
 
   return (
-    <input
-      type="number"
-      disabled={disabled}
-      value={rating}
-      onChange={onChange}
-    />
+
+    <>
+      <input
+        type="number"
+        disabled={disabled}
+        value={rating}
+        onChange={onChange}
+        />
+      <div className="rating-input">
+        {/* HOW TO DRY */}
+        {/* {for(let i = 1; i <= 5; i++){
+          <div className={rating < i ? "empty" : "filled"} >
+            <i className="fa fa-paw"></i>
+          </div>
+        }} */}
+
+        <div className={rating < 1 ? "empty" : "filled"} >
+          <i className="fa fa-paw"></i>
+        </div>
+        <div className={rating < 2 ? "empty" : "filled"} >
+          <i className="fa fa-paw"></i>
+        </div>
+        <div className={rating < 3 ? "empty" : "filled"} >
+          <i className="fa fa-paw"></i>
+        </div>
+        <div className={rating < 4 ? "empty" : "filled"} >
+          <i className="fa fa-paw"></i>
+        </div>
+        <div className={rating < 5 ? "empty" : "filled"} >
+          <i className="fa fa-paw"></i>
+        </div>
+      </div>
+    </>
   );
 };
 
